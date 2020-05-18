a41bf7ccbf5e79839fb979e40fc8baefbd29c2fe
Brian Tran <98brian.tran@gmail.com>
2020-05-18 04:17:48 +0000

    Added the UI for the bar and x and y axis Also added an input and start screen before the bar shows up No actual back end has been implemented to take in any date yet though

--- assets/drawBarChart.js
+++ assets/drawBarChart.js
@@ -1,5 +1,35 @@
+let tempInputData = "";
 $(document).ready(function(){
-  $("button").click(function(){
-    $("p").hide("fast");
+  $("#getStarted").click(function(){
+    $(".start").hide();
+    $("#barGraph").show("2");
+  });
+
+  $("#barWidth").keypress(function (event) {
+    let keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      $(".bar").css("max-width", (document.getElementById("barWidth").value + "%"))
+      $("#bar1").text(document.getElementById("barWidth").value)
+    }
+  });
+
+  $("input").focus(function() {
+    $(this).val("");
+    $(this).css("color", "black");
+    tempInputData = this.value;
+  });
+  $("input").blur(function() {
+    if (this.value == "") {
+      $(this).css("color", "gray")
+      $(this).val(tempInputData)
+    }
   });
 });
+
+
+function drawBarChart(data, options, element) {
+  let array = [];
+  array = data;
+  let object = {width:0, height:0};
+  let string = element;
+};
